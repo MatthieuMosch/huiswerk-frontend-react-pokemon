0881063c9240a7c3aec8dfc11d0fc5e852abb318
MatthieuMosch <mmosch@gmail.com>
2025-03-25 16:07:52 +0100

    abort controller toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -4,73 +4,58 @@ import './App.css'
 
 import Button from "./components/button/Button.jsx";
 import Card from "./components/card/Card.jsx";
-import makeUri20 from "./helpers/makeUri20.jsx";
 import InfoBox from "./components/infobox/InfoBox.jsx";
 
 function App() {
-    const uri = "https://pokeapi.co/api/v2/";
     const [loading, setLoading] = useState(false);
     const [errorMsg, setErrorMsg] = useState("");
-    const [pokemon, setPokemon] = useState({});
     const [pokemons, setPokemons] = useState([]);
     const [uri20, setUri20] = useState("https://pokeapi.co/api/v2/pokemon");
     const [uriPrev, setUriPrev] = useState(null);
     const [uriNext, setUriNext] = useState(null);
 
     useEffect(() => {
-        async function fetchOnePokemon(name) {
-            setLoading(true);
-            try {
-                const response = await axios.get(`${uri}/pokemon/${name}`);
-                console.log(response.data);
-                setPokemon(response.data);
-            } catch (err) {
-                console.error(err);
-                setErrorMsg(err.message);
-            } finally {
-                setLoading(false);
-            }
-        }
-        void fetchOnePokemon("zubat");
-    }, []);
-
-    useEffect(() => {
+        const controller = new AbortController();
         const fetch20Pokemon = async () => {
             setLoading(true);
             try {
-                const response = await axios.get(uri20);
+                const response = await axios.get(uri20, {signal: controller.signal});
                 setUriPrev(response.data.previous);
                 setUriNext(response.data.next);
                 setPokemons(response.data.results);
             } catch (err) {
-                console.error(err);
-                setErrorMsg(err.message);
+                if (axios.isCancel(err)) {
+                    console.log("Request: ", err.message);
+                } else {
+                    console.error(err);
+                    setErrorMsg(err.message);
+                }
             } finally {
                 setLoading(false);
             }
         }
         fetch20Pokemon();
+        return () => {controller.abort();}
     }, [uri20])
 
     return (
         <>
+            {loading && <InfoBox type="info">Loading...</InfoBox>}
+            {errorMsg && <InfoBox type="error">{errorMsg}</InfoBox>}
             <h1>Gotta catch em all!</h1>
-            {loading &&
-                <InfoBox type="info">Loading...</InfoBox>
-            }
-            {errorMsg &&
-                <InfoBox type="error">{errorMsg}</InfoBox>
-            }
-            <Card pokemon={pokemon}/>
-            <Button disabled={!uriPrev} uri={uriPrev} onClick={setUri20}>Vorige</Button>
-            <Button disabled={!uriNext} uri={uriNext} onClick={setUri20}>Volgende</Button>
-            {pokemons.length > 0 &&
-                <ul>
-                    {pokemons.map((pokemon) => (
-                        <li key={pokemon.name}>{pokemon.name}</li>
-                    ))}
-                </ul>
-            }
+            <nav>
+                <Button disabled={!uriPrev} uri={uriPrev} onClick={setUri20}>Vorige</Button>
+                <Button disabled={!uriNext} uri={uriNext} onClick={setUri20}>Volgende</Button>
+            </nav>
+            <main>
+                {pokemons.length > 0 && (
+                    <ul>
+                        {pokemons.map((pokemon) => (
+                            <Card key={pokemon.name} uri={pokemon.url}/>
+                        ))}
+                    </ul>
+                )}
+            </main>
         </>
     )
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,20 +1,48 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
 import "./Card.css";
 
-function Card({pokemon}) {
+import InfoBox from "../infobox/InfoBox.jsx";
+
+function Card({uri}) {
+    const [loading, setLoading] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
+    const [details, setDetails] = useState({});
+
+    useEffect(() => {
+        const controller = new AbortController();
+        async function fetchOnePokemon() {
+            setLoading(true);
+            try {
+                const response = await axios.get(uri, {signal: controller.signal});
+                setDetails(response.data);
+            } catch (err) {
+                if (axios.isCancel(err)) {
+                    console.log("Request: ", err.message);
+                } else {
+                    console.error(err);
+                    setErrorMsg(err.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+        void fetchOnePokemon();
+        return () => {controller.abort();}
+    }, []);
+
     return (
         <article>
-            <h2>{pokemon.name}</h2>
-            {pokemon.sprites &&
-                <img src={pokemon.sprites.front_default}/>
-            }
-            {pokemon.moves &&
-                <p>moves: {pokemon.moves.length}</p>
-            }
-            <p>weight : {pokemon.weight}</p>
+            {loading && <InfoBox type="info">Loading...</InfoBox>}
+            {errorMsg && <InfoBox type="error">{errorMsg}</InfoBox>}
+            <h2>{details.name}</h2>
+            {details.sprites && <img src={details.sprites.front_default}/>}
+            {details.moves && <p>moves: {details.moves.length}</p>}
+            <p>weight : {details.weight}</p>
             <p>abilities:</p>
-            {pokemon.abilities &&
+            {details.abilities &&
                 <ul>
-                    {pokemon.abilities.map((ability) => (
+                    {details.abilities.map((ability) => (
                         <li key={ability.ability.name}>{ability.ability.name}</li>
                     ))}
                 </ul>}
